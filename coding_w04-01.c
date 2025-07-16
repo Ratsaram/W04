5a9b5e286bef2386d945e668af34811fd97ab361
Ratsaram <s6812022610565@email.kmutnb.ac.thgit>
2025-07-16 17:17:42 +0700

    eiei

---
+++ coding_w04-01.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main () {   // int(keyword) main(Identifiers)
+    printf("Hello "); // printf(Identifier)  ;(Operator)
+    printf("C Tokens ! "); // printf(Identyifier) ;(operator)
+    return 0;  // return(keyword) ;(Operator)
+
+}
---
+++ coding_w04-02.c
@@ -0,0 +1,7 @@
+#include <stdio.h> // (ไม่มี # หน้าinclude) (ไม่มี<>หน้าและหลังstdio.h)
+
+int main(){
+    int a = 10;  // ไม่มี ; ลงท้าย
+    printf("a = %d", a); // ไม่มี ; ลงท้าย
+    return 0; // ไม่มี ; ลงท้าย
+}
